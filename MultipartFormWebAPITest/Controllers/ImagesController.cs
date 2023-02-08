9392c2b611b8e093299db126880bc957b7a1d989
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-02-09 01:05:58 +0200

    Add project files.

---
+++ MultipartFormWebAPITest/Controllers/ImagesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MultipartFormWebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // GET: api/<ImagesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(500);
+        }
+
+        // GET api/<ImagesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return StatusCode(500);
+        }
+
+        // POST api/<ImagesController>
+        [HttpPost]
+        public IActionResult Post([FromForm] IFormFileCollection files)
+        {
+            if (!Request.ContentType!.StartsWith("multipart/form-data") || files is null)
+            {
+                return new UnsupportedMediaTypeResult();
+            }
+
+            if (files.Count == 0)
+            {
+                return BadRequest();
+            }
+
+            using var readStream = files[0].OpenReadStream();
+
+            var bytes = new byte[readStream.Length];
+
+            readStream.Read(bytes);
+
+            return File(bytes, files[0].ContentType);
+        }
+
+        // PUT api/<ImagesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return StatusCode(500);
+        }
+
+        // DELETE api/<ImagesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return StatusCode(500);
+        }
+    }
+}
---
+++ MultipartFormWebAPITest/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Mvc And Razor Pages here
+builder.Services.AddMvc();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add Routing and Endpoints here
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+});
+
+
+app.Run();
